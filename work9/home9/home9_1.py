6da1febcf054930b15dba5bd11ddc1869d6fdf3c
obetsa <obetsa@gmail.com>
2021-06-14 23:30:28 +0300

    hw9

--- work9/home9/home9_1.py
+++ work9/home9/home9_1.py
@@ -12,3 +12,36 @@ json-файл с именем вида <id записи>.json. Если id не 
 создавать/изменять/удалять новую заявку/сотрудника/департамент
 Подсказка: у вас должно получиться 3 модели и 9 функций =)
 """
+
+import mongoengine as me
+import json
+from datetime import datetime as dt
+
+
+me.connect("LESSON")
+
+
+class UserProfile(me.Document):
+    login = me.StringField(required=True)
+    password = me.StringField(required=True)
+    like = me.IntField(default=0)
+
+    def __str__(self):
+        return f"login: {self.login} | password: {self.password}"
+
+
+# first_profile = UserProfile(login='Andrii', password='123')
+# first_profile = UserProfile(login='sdfsd', password='sdf435')
+first_profile = UserProfile(login='dsfdsfsdfs', password='123555')
+first_profile.save()
+
+# res = UserProfile.objects.all()
+# for item in res:
+#     print(item)
+#     js_data = item.to_json()
+#     print(js_data, type(js_data))
+#     dict_data = json.loads(item.to_json())
+#     print(dict_data, type(dict_data))
+
+res = UserProfile.objects.get(pk="60c766512efb3b840575959f")
+print(res)
