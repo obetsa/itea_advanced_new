a91d0784db7db945b1444e3b9fa7cc16660a54a7
obetsa <obetsa@gmail.com>
2021-07-16 13:58:22 +0300

    hw14 wokring with migrate

--- work14/home14/home14_1.py
+++ work14/home14/home14_1.py
@@ -1,14 +1,20 @@
 """Перевести все существующие модели (Заявки, Департаменты, Сотрудники) на Flask-SQLAlchemy.
 Перевести все ручки Flask-приложения из прошлого ДЗ на использование этих моделей.
 """
+from flask_script import Manager
+from flask_migrate import Migrate, MigrateCommand
 
 from flask import Flask, request, render_template, Response
 from flask_sqlalchemy import SQLAlchemy
 import json
 from datetime import datetime
 
-DB_URL = "postgresql://postgres:12345678@localhost:5432/order_service_db"
+import psycopg2
+from psycopg2 import sql
+
+DB_URL = "postgresql://postgres:12345678@localhost:5432/postgres"
 # order_service_db
+# connect = psycopg2.connect(DB_URL)
 
 app = Flask(__name__)
 
@@ -18,10 +24,17 @@ app.config['SQLALCHEMY_DATABASE_URI'] = DB_URL
 db = SQLAlchemy(app)
 # db.create_all()
 
+migrate = Migrate(app, db)
+manager = Manager(app)
+
+manager.add_command('db', MigrateCommand)
+
+# SELECT_QUERY_departments = """SELECT * FROM order_service_db.departments"""
 
 class Departments(db.Model):
     department_id = db.Column(db.Integer, primary_key=True, autoincrement=True)
     department_name = db.Column(db.String(75), unique=True)
+
 
 
 class Employees(db.Model):
@@ -42,6 +55,14 @@ class Orders(db.Model):
     creator_id = db.Column(db.Integer, db.ForeignKey('employees.employee_id'), nullable=False)
 
 
+# def select_data():
+#     with connect, connect.cursor() as cursor:
+#         cursor.execute(SELECT_QUERY_departments)
+#         result = cursor.fetchall()
+#         for r in result:
+#             print(r[0])
+
+
 @app.route('/')
 def index():
     return render_template("index.html")
@@ -51,7 +72,7 @@ def dep_get_dict(d):
     return {"Name": d.department_name, "Id": d.department_id, "Create date": str(d.create_dt)}
 
 
-@app.route("/departments/all", methods=["GET"])
+@app.route("/departments/all", methods=["POST", "GET"])
 def dep_get_data():
     return render_template('department.html', lst=[dep_get_dict(i) for i in Departments.query.all()])
 
@@ -69,11 +90,12 @@ def create_departments():
         return Response("Ничего не найдено", status=404)
 
 
-@app.route('/create_employee', methods=['POST'])
+@app.route('/create_employee', methods=["POST", "GET"])
 def create_employee():
     if request.method == 'POST':
         employees_data = json.loads(request.data)
-        employee_profile = Employees(fio=employees_data['fio'],
+        employee_profile = Employees(employee_id=employees_data['employee_id'],
+                                     fio=employees_data['fio'],
                                      position=employees_data['position'],
                                      department_id=employees_data['department_id'])
         db.session.add(employee_profile)
@@ -84,7 +106,7 @@ def create_employee():
         return Response("Ничего не найдено", status=404)
 
 
-@app.route('/create_order', methods=['POST'])
+@app.route('/create_order', methods=["POST", "GET"])
 def create_order():
     if request.method == 'POST':
         order_data = json.loads(request.data)
@@ -103,5 +125,10 @@ def create_order():
         return Response("Ничего не найдено", status=404)
 
 
+# select_data()
+
+
 if __name__ == '__main__':
-    app.run(debug=True)
+    # app.run(debug=True)
+    manager.run()
+
