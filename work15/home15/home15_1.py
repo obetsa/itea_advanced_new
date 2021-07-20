c173385f86fe2f84d4286542895ecfcf1da7c556
obetsa <obetsa@gmail.com>
2021-07-20 12:21:54 +0300

    hw15 done(problem: sqlalchemy.exc.OperationalError: (psycopg2.OperationalError))

--- work15/home15/home15_1.py
+++ work15/home15/home15_1.py
@@ -60,16 +60,121 @@ class Orders(db.Model):
     status = db.Column(db.String(10), nullable=False)
     serial_no = db.Column(db.Integer, nullable=False, unique=True)
     creator_id = db.Column(db.Integer, db.ForeignKey('employees.employee_id'), nullable=False)
+    customer_id = db.Column(db.Integer, db.ForeignKey('customers.customer_id'))
 
 
 class Customers(db.Model):
     customer_id = db.Column(db.Integer, primary_key=True, autoincrement=True)
     created_dt = db.Column(db.DateTime)
-    name = db.Column(db.String(50))
+    c_name = db.Column(db.String(50))
+    chat_id = db.Column(db.Integer)
     password = db.Column(db.Integer)
     phone = db.Column(db.Integer)
     email = db.Column(db.String(20))
     is_subscribed = db.Column(db.Boolean, default=False)
+
+
+class NotificationTasks(db.Model):
+    notification_task_id = db.Column(db.Integer, primary_key=True, autoincrement=True)
+    create_dt = db.Column(db.DateTime, default=datetime.now())
+    message = db.Column(db.String(100))
+    name = db.Column(db.Integer)
+    profile_tg_chat_id = db.Column(db.String(10), nullable=True)
+
+
+@app.route("/create_departments", methods=["POST"])
+def create_departments():
+    if request.method == 'POST':
+        departments_data = json.loads(request.data)
+        department_profile = Departments(department_name=departments_data["department_name"])
+        db.session.add(department_profile)
+        db.session.flush()
+        db.session.commit()
+        return 'OK'
+
+
+@app.route('/search_department_by_id/<string:search_by_id>', methods=['GET'])
+def search_department_by_id(search_by_id):
+    search_departments = Departments.query.filter_by(department_id=search_by_id).first()
+    return f'OK: {search_departments}'
+
+
+@app.route('/delete_department_id/<string:delete_by_id>', methods=['DELETE'])
+def delete_department_id(delete_by_id):
+    delete_id = Departments.query.filter_by(department_id=delete_by_id).first()
+    db.session.delete(delete_id)
+    db.session.commit()
+    return f'OK, department: {delete_by_id} deleted'
+
+
+@app.route('/create_employee', methods=["POST", "GET"])
+def create_employee():
+    if request.method == 'POST':
+        employees_data = json.loads(request.data)
+        employee_profile = Employees(employee_id=employees_data['employee_id'],
+                                     fio=employees_data['fio'],
+                                     position=employees_data['position'],
+                                     department_id=employees_data['department_id'])
+        db.session.add(employee_profile)
+        db.session.flush()
+        db.session.commit()
+        return 'OK'
+
+
+@app.route('/search_employees_by_id/<string:search_by_id>', methods=['GET'])
+def search_employees_by_id(search_by_id):
+    search_employees = Employees.query.filter_by(employees_id=search_by_id).first()
+    return f'OK: {search_employees}'
+
+
+@app.route('/delete_employees_id/<string:delete_by_id>', methods=['DELETE'])
+def delete_employees_id(delete_by_id):
+    delete_id = Employees.query.filter_by(employee_id=delete_by_id).first()
+    db.session.delete(delete_id)
+    db.session.commit()
+    return f'OK, employees: {delete_by_id} deleted'
+
+
+@app.route('/create_order', methods=["POST", "GET"])
+def create_order():
+    if request.method == 'POST':
+        order_data = json.loads(request.data)
+        order_profile = Orders(create_dt=order_data['create_dt'],
+                               order_type=order_data['order_type'],
+                               description=order_data['description'],
+                               status=order_data['status'],
+                               serial_no=order_data['serial_no'],
+                               creator_id=order_data['creator_id'],
+                               )
+        db.session.add(order_profile)
+        db.session.flush()
+        db.session.commit()
+        return 'OK'
+
+
+@app.route('/search_order_by_id/<string:search_by_id>', methods=['GET'])
+def search_order_by_id(search_by_id):
+    search_order = Orders.query.filter_by(order_id=search_by_id).first()
+    return f'OK: {search_order}'
+
+
+@app.route('/change_order_status', methods=['PATCH'])
+def change_order_status():
+    if request.method == 'PATCH':
+        order_status = json.loads(request.data)
+        get_id = Orders.query.filter_by(order_id=order_status['order_id']).first()
+        get_id.status = order_status['Closed']
+        get_id.update_dt = datetime.now()
+        db.session.commit()
+        return f'OK: Status changed'
+
+
+@app.route('/delete_order_id/<string:delete_by_id>', methods=['DELETE'])
+def delete_order_id(delete_by_id):
+    delete_id = Orders.query.filter_by(order_id=delete_by_id).first()
+    db.session.delete(delete_id)
+    db.session.commit()
+    return f'OK, order: {delete_by_id} deleted'
 
 
 if __name__ == '__main__':
--- work15/home15/telegra_bot.py
+++ work15/home15/telegra_bot.py
@@ -1,13 +1,24 @@
 import telebot
-from telebot import TeleBot
+
+from home15_1 import Customers, NotificationTasks, db
+from secrets import token_hex
+
+token = token_hex()
+print(token)
 
 api_telegram = '1517223699:AAG4JUoO3NMfiOoch9WkDuTa4MDdQ4g-csk'
 bot = telebot.TeleBot(api_telegram)
 
+
+@bot.message_handler(commands=['start'])
+def welcome(message):
+    name = message.from_user.first_name
+    bot.send_message(message.chat.id, 'Hello, ' + str(name))
+
+
 @bot.message_handler(commands=["subscribe_me"])
 def subscribe_profile(message):
-    """Подписываем пользака на уведомления"""
-    profile = Profiles.query.get(int(message.text.split()[1]))
+    profile = Customers.query.get(int(message.text.split()[1]))
     profile.is_subscribed = True
     profile.profile_tg_chat_id = message.chat.id
     db.session.commit()
@@ -15,20 +26,46 @@ def subscribe_profile(message):
 
 @bot.message_handler(commands=["unsubscribe_me"])
 def unsubscribe_profile(message):
-    """Отписываем пользака от уведомлений"""
-    profile = Profiles.query.get(int(message.text.split()[1]))
+    profile = Customers.query.get(int(message.text.split()[1]))
     profile.is_subscribed = False
     profile.profile_tg_chat_id = None
     db.session.commit()
+
+
+def order_create(message):
+    name = message.from_user.first_name
+    bot.reply_to(message, str(name) + ', Create your order: ')
+    order_add = NotificationTasks(name=message.text, notification_task_id=message.chat.id,
+                                  message=message.text, create_dt=message.dt)
+    db.session.add(order_add)
+    db.session.flush()
+    db.session.commit()
+    bot.reply_to(message, 'Order created')
+
+
+@bot.message_handler(commands=['create'])
+def order_message(message):
+    custom = [i.c_name for i in Customers.query.all()]
+    if message.from_user.first_name not in custom:
+        user = Customers(c_name=message.from_user.first_name, phone=message.text, create_dt=message.dt,
+                         chat_id=message.chat.id)
+        db.session.add(user)
+        db.session.commit()
+        bot.send_message(chat_id=message.chat.id, text="Register")
+    else:
+        bot.send_message(chat_id=message.chat.id, text="Registered in base")
+
+    bot.register_next_step_handler(message, order_create)
+
 
 cnt = 0
 
 
 def my_shiny_func(message):
     global cnt
-    if cnt < 3:
+    if cnt < 1:
         print("Я принял сообщение!")
-        bot.reply_to(message=message, text=f"{message.chat.username}, ты выбрал {message.text}")
+        bot.reply_to(message=message, text=f"{message.from_user.first_name}, ты выбрал {message.text}")
         bot.register_next_step_handler(message, my_shiny_func)
         cnt += 1
 
@@ -41,13 +78,13 @@ def planner(message):
 def echo(message):
     global cnt
     cnt = 0
-    bot.reply_to(message=message, text=f"{message.chat.username}, давай запланируемся на 3 дней?")
+    bot.reply_to(message=message, text=f"{message.from_user.first_name}, давай запланируемся на 3 дней?")
     bot.register_next_step_handler(message, my_shiny_func)
 
 
 @bot.message_handler(commands=["echo"])
 def echo(message):
-    bot.reply_to(message=message, text=f"{message.chat.username}, выбери одно из двух: курица или рыба?")
+    bot.reply_to(message=message, text=f"{message.from_user.first_name}, выбери одно из двух: заявки или инфо?")
     bot.register_next_step_handler(message, my_shiny_func)
 
 
@@ -58,14 +95,12 @@ def notify(chat_id):
 
 @bot.message_handler(commands=["notify_all"])
 def notify_all(message):
-    """Хороший способ для оповещения всех пользователей по команде от админа"""
     _, user_token = message.text.split()
     if user_token == token:
         print("Я пошел в базу данных и вытащил всех, кто подписан на уведомления")
-        profiles = Profiles.query.filter_by(is_subscribed=True)
+        profiles = Customers.query.filter_by(is_subscribed=True)
         for profile in profiles:
             notify(chat_id=profile.profile_tg_chat_id)
-
 
 
 if __name__ == "__main__":
